a87ebeb451f71e0f00fcac497cf51a938894334c
vyoum <126673819+Vyoum@users.noreply.github.com>
2025-11-30 13:35:05 +0530

    curr

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import PortfolioSections from "@/components/PortfolioSections";
+import CurriculumSection from "@/components/CurriculumSection";
 
 export default function Home() {
   return (
